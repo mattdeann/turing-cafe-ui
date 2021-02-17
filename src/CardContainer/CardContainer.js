018ee5a8a457a5b0a11bf746f96dfeedf4361745
mattdean <matthewallandean@gmail.com>
2021-02-17 09:24:28 -0700

    Finish initial fetch

--- src/CardContainer/CardContainer.js
+++ src/CardContainer/CardContainer.js
@@ -7,10 +7,20 @@ class CardContainer extends Component {
   constructor() {
     super()
     this.state = {
+      // data is an array
       data: null
     }
   }
 
+  componentDidMount() {
+    fetch("http://localhost:3001/api/v1/reservations")
+    .then(response => response.json())
+    .then(response  =>
+      this.setState({data: response})
+    )
+  }
+
+  // to render the cards, we want to iterate over the array and create a card from each array element
 
   render() {
     return (
