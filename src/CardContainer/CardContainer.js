565ba7ba13c9835b1cad880f3fda1187d25769c5
mattdean <matthewallandean@gmail.com>
2021-02-17 09:12:39 -0700

    Set initial file structure

---
+++ src/CardContainer/CardContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CardContainer extends Component {
+  constructor() {
+    super()
+    this.state = {
+      data: null
+    }
+  }
+
+}
+
+export default CardContainer;
